1d6fa314978822e30ad75ac4399aed45b3e99f0d
Funsho <tuninissi@gmail.com>
2025-04-07 16:44:00 +0100

    fixed tailwind bug

--- onboarding-checklist/src/components/Employee.tsx
+++ onboarding-checklist/src/components/Employee.tsx
@@ -1,15 +1,28 @@
+// import { useState } from "react"
 
 
 function Employee() {
+
+    // const[name, setName] = useState("")
+    // const[email, setEmail] = useState("")
+    // const[jobROle, setJobRole] = useState("")
+    // const[startDate, setStartDate] = useState("")
+
+
   return (
     <>
     <div>
         <form style={{width: '350px'}}>
-            <input className="" type="text" name="fullname" placeholder="Full name"/>
-            <input type="email" name="email" placeholder="Email"/>
-            <input type="text" name="jobrole" placeholder="Job Role"/>
-            <input type="date" name="startdate"/>
+            <h2 className="text-2xl font-bold mb-6 text-grey-800">Enter new Employee details</h2>
+            <input className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" type="text" name="fullname" placeholder="Full name"/>
+            <input className="my-2 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" type="email" name="email" placeholder="Email"/>
+            <input className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" type="text" name="jobrole" placeholder="Job Role"/>
+            <input className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" type="date" name="startdate"/>
         </form>
+
+        <div>
+            
+        </div>
     </div>
     </>
   )
